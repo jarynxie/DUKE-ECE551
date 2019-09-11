33998bdf29b8d3e3f8ca4a59e7c6cebbb335e115
jx95 <jx95@duke.edu>
2019-09-11 14:42:50 -0400

    completed 027

---
+++ 027_tests_power/test-power.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+void run_check(unsigned x, unsigned y, unsigned expected_ans) {
+  unsigned ans = power(x, y);
+  if (expected_ans != ans) {
+    printf("There is an error!");
+    exit(EXIT_FAILURE);
+  }
+}
+int main(void) {
+  run_check(0, 0, 1);
+  run_check(0, 1, 0);
+  run_check(3, 3, 27);
+  run_check(-1, 2, 1);
+  run_check(10, 0, 1);
+  run_check(1, 0, 1);
+  run_check(1, -2, 1);
+  run_check(2, 31, 2147483648);
+  return EXIT_SUCCESS;
+}
