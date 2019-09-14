9d598eb8e7062ab16653f359fa6643b69292a1c7
jx95 <jx95@duke.edu>
2019-09-14 01:34:23 -0400

    completed printSeq1Range and its test cases in main

--- 029_num_seq/step1.c
+++ 029_num_seq/step1.c
@@ -13,9 +13,21 @@ int seq1(int x) {
   return 4 * x - 3;
 }
 
+void printSeq1Range(int low, int high) {
+  if (low >= high) {
+    printf("\n");
+  }
+  else {
+    for (int current = low; current < high - 1; current++) {
+      printf("%d, ", seq1(current));
+    }
+    printf("%d\n", seq1(high - 1));
+  }
+}
+
 int main(void) {
-  int seq1In;
-  int seq1Result;
+  int seq1In = 0;
+  int seq1Result = 0;
 
   seq1In = 0;
   seq1Result = seq1(seq1In);
@@ -68,4 +80,52 @@ int main(void) {
   seq1In = 1000000000;
   seq1Result = seq1(seq1In);
   printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  int low = 0;
+  int high = 0;
+
+  low = 0;
+  high = 4;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = 5;
+  high = 9;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = -2;
+  high = 6;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = -10;
+  high = -2;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = 7;
+  high = 3;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = 30;
+  high = 25;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = 0;
+  high = 0;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = -500000000;
+  high = -499999995;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
+
+  low = 500000000;
+  high = 500000005;
+  printf("printSeq1Range(%d, %d)\n", low, high);
+  printSeq1Range(low, high);
 }
