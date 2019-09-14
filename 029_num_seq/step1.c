5eebacc4e18ea18d5791c34d4abdded1326c4cf2
jx95 <jx95@duke.edu>
2019-09-14 00:51:10 -0400

    completed seq1 and its test cases in main

--- 029_num_seq/step1.c
+++ 029_num_seq/step1.c
@@ -6,3 +6,66 @@
 //  Step 1 (D): add test cases to main to test printSeq1Range
 //
 // Be sure to #include any header files you need!
+#include <stdio.h>
+#include <stdlib.h>
+
+int seq1(int x) {
+  return 4 * x - 3;
+}
+
+int main(void) {
+  int seq1In;
+  int seq1Result;
+
+  seq1In = 0;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = -1;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = 1;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = 9;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = 10;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = 11;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = -500000000;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = 500000000;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = -2;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = -3;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = -4;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = -1000000000;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+
+  seq1In = 1000000000;
+  seq1Result = seq1(seq1In);
+  printf("seq1(%d) = %d\n", seq1In, seq1Result);
+}
