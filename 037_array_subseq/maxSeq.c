ad7e5f347c0165b1b3e9f023e6e65d49e432aaa7
jx95 <jx95@duke.edu>
2019-09-16 21:10:31 -0400

    completed 034

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq_helper(int * curLoc, size_t curMax) {
+  size_t returnMax = curMax;
+  if (*curLoc <= *(curLoc - 1)) {
+    returnMax = 1;
+  }
+  else {
+    returnMax++;
+  }
+  return returnMax;
+}
+
+size_t maxCal(size_t a, size_t b) {
+  return a > b ? a : b;
+}
+
+size_t maxSeq(int * array, size_t n) {
+  if (n == 0) {
+    return 1;
+  }
+  size_t max = 1;
+  size_t curMax = 1;
+  for (size_t i = 1; i < n; i++) {
+    curMax = maxSeq_helper(array + i, curMax);
+    max = maxCal(curMax, max);
+  }
+  return max;
+}
