017f8ab48822614a19693b35d52f319c5c5f55b4
jx95 <jx95@duke.edu>
2019-09-23 17:30:02 -0400

    047 forth commit

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -32,7 +32,7 @@ int main(int argc, char ** argv) {
   size_t frequencyArray[26] = {0};
   FILE * f = fopen(argv[1], "r");
   if (f == NULL) {
-    perror("Could not open file");
+    fprintf(stderr, "Could not open file");
     return EXIT_FAILURE;
   }
   decrypt(f, frequencyArray);
@@ -41,7 +41,7 @@ int main(int argc, char ** argv) {
   int key = answer - 'e';
   printf("%d\n", key % 26);
   if (fclose(f) != 0) {
-    perror("Failed to close the input file!");
+    fprintf(stderr, "Failed to close the input file!");
     return EXIT_FAILURE;
   }
   return EXIT_SUCCESS;
