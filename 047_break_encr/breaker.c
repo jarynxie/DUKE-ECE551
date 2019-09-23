7064dd1af60938619b663b2c5bf8409f59c34957
jx95 <jx95@duke.edu>
2019-09-23 14:35:56 -0400

    047 first commit

---
+++ 047_break_encr/breaker.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int arrayMax(size_t * array, int n) {
+  if (n == 0) {
+    return -1;
+  }
+  size_t largest = array[0];
+  for (int i = 1; i < n; i++) {
+    if (array[i] > largest) {
+      largest = i;
+    }
+  }
+  return largest;
+}
+
+void decrypt(FILE * f, size_t array[]) {
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      array[c - 97]++;
+    }
+  }
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: inputFileName\n");
+    return EXIT_FAILURE;
+  }
+  size_t frequencyArray[26] = {0};
+  FILE * f = fopen(argv[1], "r");
+  decrypt(f, frequencyArray);
+  int largest = arrayMax(frequencyArray, 26);
+  size_t answer = largest + 97;
+  int key = answer - 'e';
+  printf("%d\n", key % 26);
+}
