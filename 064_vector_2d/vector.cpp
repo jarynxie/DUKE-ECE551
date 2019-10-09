224cab0a10b567a969b2fa11c7d005bd8f40c914
jx95 <jx95@duke.edu>
2019-10-09 13:43:38 -0400

    completed 064

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -3,5 +3,21 @@
 #include <cmath>
 #include <cstdio>
 
-/* write your class implementation in this file
- */
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D returnVector;
+  returnVector.x = 0;
+  returnVector.y = 0;
+  returnVector.x = x + rhs.x;
+  returnVector.y = y + rhs.y;
+  return returnVector;
+}
+
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+double Vector2D::dot(const Vector2D & rhs) const {
+  return x * rhs.x + y * rhs.y;
+}
