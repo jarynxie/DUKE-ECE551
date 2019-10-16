f8e582d4786c8933258732ae0c51bc7971207c5f
jx95 <jx95@duke.edu>
2019-10-16 09:53:19 -0400

    completed 070

---
+++ 070_point/point.cpp
@@ -0,0 +1,16 @@
+#include "point.hpp"
+
+#include <cmath>
+#include <cstdio>
+
+void Point::move(double dx, double dy) {
+  x += dx;
+  y += dy;
+}
+
+double Point::distanceFrom(const Point & p) {
+  double answer;
+  answer = (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
+  answer = sqrt(answer);
+  return answer;
+}
