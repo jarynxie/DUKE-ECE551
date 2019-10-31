86d9e7b65386f8238d66d7b20d4b44d66ae6ef81
jx95 <jx95@duke.edu>
2019-10-31 13:29:05 -0400

    updated 092

--- 092_lls/ll.h
+++ 092_lls/ll.h
@@ -35,13 +35,13 @@ class LinkedList {
   }
   LinkedList & operator=(const LinkedList & rhs) {
     if (this != &rhs) {
-      LinkedList<T> temp = new LinkedList<T>();
+      LinkedList<T> * temp = new LinkedList<T>();
       for (int i = 0; i < rhs.getSize(); i++) {
-        temp.addBack(rhs[i]);
+        *temp->addBack(rhs[i]);
       }
-      this.~LinkedList();
-      temp.~LinkedList();
-      this = temp;
+      *this.~LinkedList();
+      *temp.~LinkedList();
+      *this = temp;
     }
     return *this;
   }
