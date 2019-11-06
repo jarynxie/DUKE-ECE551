d98665a365de2ed70da8e4645b9da5b835636f9c
jx95 <jx95@duke.edu>
2019-11-05 22:35:05 -0500

    completed 096

--- 096_binsrch/search.cpp
+++ 096_binsrch/search.cpp
@@ -3,30 +3,16 @@
 
 #include "function.h"
 
-int searchHelper(Function<int, int> * f, int low, int high) {
-  int curr = (low + high) / 2;
-  int fx = f->invoke(curr);
-  if (fx == 0) {
-    return curr;
-  }
-  if (fx > 0) {
-    return searchHelper(f, low, curr);
-  }
-  if (fx < 0) {
-    return searchHelper(f, curr + 1, high);
-  }
-  return low;
-}
-
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
   if (low == high || high - low == 1 || low > high) {
     return low;
   }
-  if (f->invoke(high - 1) < 0) {
-    return high - 1;
+  int middle = (low + high) / 2;
+  int fx = f->invoke(middle);
+  if (fx < 0) {
+    return binarySearchForZero(f, middle, high);
   }
-  if (f->invoke(low) > 0) {
-    return low;
+  else {
+    return binarySearchForZero(f, low, middle);
   }
-  return searchHelper(f, low, high);
 }
