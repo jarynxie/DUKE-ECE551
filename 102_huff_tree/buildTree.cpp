aa61b15e74b3c3bfab4ef8fe77c9acd1c835ce67
jx95 <jx95@duke.edu>
2019-11-11 21:35:11 -0500

    completed 102

--- 102_huff_tree/buildTree.cpp
+++ 102_huff_tree/buildTree.cpp
@@ -1,5 +1,18 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
-}  
+Node * buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for (uint64_t i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node * temp1 = pq.top();
+    pq.pop();
+    Node * temp2 = pq.top();
+    pq.pop();
+    pq.push(new Node(temp1, temp2));
+  }
+  return pq.top();
+}
