e86d34482509922087387699273b0a1aa6b09c0b
jx95 <jx95@duke.edu>
2019-11-22 18:33:08 -0500

    completed search, but valgrind reported errors

--- mp_miniproject/ffosh.cpp
+++ mp_miniproject/ffosh.cpp
@@ -7,7 +7,37 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <vector>
 using namespace std;
+
+void readCin(const char *& result) {
+  string resultStr;
+  getline(cin, resultStr);
+  result = resultStr.c_str();
+}
+
+void searchVector(const char *& cinRead, char *& environ, vector<string> & search) {
+  string envString(environ);
+  stringstream envStream(envString);
+  stringstream cinStream(cinRead);
+  string temp = "";
+  while (!envStream.eof()) {
+    getline(envStream, temp, ':');
+    search.push_back(temp);
+  }
+  string fileName = "";
+  getline(cinStream, fileName, ' ');
+  if (fileName.find('/') == string::npos) {
+    vector<string>::iterator it = search.begin();
+    while (it != search.end()) {
+      it->push_back('/');
+      it->append(fileName);
+      it++;
+    }
+  }
+}
+
 int main(int argc, char * argv[]) {
   if (argc != 1) {
     cout << "Usage: ./ffosh\n";
@@ -21,24 +51,29 @@ int main(int argc, char * argv[]) {
     int wstatus;
     cout << "ffosh$ ";
     char * newargv[] = {NULL, NULL};
-    string readString;
-    getline(cin, readString);
-    char * readResult = new char[readString.length() + 1];
-    strcpy(readResult, readString.c_str());
-    if (strcmp(readResult, "exit") == 0) {
+    const char * cinCharStar = "";
+    readCin(cinCharStar);
+    if (strcmp(cinCharStar, "exit") == 0) {
       exit(EXIT_SUCCESS);
     }
-    if (strcmp(readResult, "") == 0) {
+    if (strcmp(cinCharStar, "") == 0) {
       continue;
     }
-    newargv[0] = readResult;
+    vector<string> search;
+    searchVector(cinCharStar, newenviron[0], search);
     cPid = fork();
     if (cPid == -1) {
       perror("fork");
       exit(EXIT_FAILURE);
     }
     if (cPid == 0) {
-      execve(newargv[0], newargv, newenviron);
+      vector<string>::iterator it = search.begin();
+      while (it != search.end()) {
+        const char * temp = it->c_str();
+        newargv[0] = (char *)temp;
+        execve(newargv[0], newargv, newenviron);
+        it++;
+      }
       perror("execve");
       exit(EXIT_FAILURE);
     }
